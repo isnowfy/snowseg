9392186749188599ed2c3526d22bbbad8a9fca99
isnowfy <isnowfy@gmail.com>
2013-05-08 00:07:54 +0800

    frequency add add_one_prob

--- frequency.py
+++ frequency.py
@@ -1,1 +1,34 @@
 # -*- coding: utf-8 -*-
+
+class BaseProb(object):
+    
+    def __init__(self):
+        self.d = {}
+        self.total = 0
+        self.d['_none_'] = 0
+
+    def exists(self, key):
+        return key in self.d
+
+    def getsum(self):
+        return self.total
+
+    def get(self, key):
+        if not self.exists(key):
+            return False, d['_none_']
+        return True, d[key]
+
+
+class AddOneProb(BaseProb):
+
+    def __init__(self):
+        self.d = {}
+        self.total = 0
+        self.d['_none_'] = 1
+
+    def add(self, key, value):
+        self.total += value+1
+        if not self.exists(key):
+            d[key] = 0
+        else:
+            d[key] += value+1
